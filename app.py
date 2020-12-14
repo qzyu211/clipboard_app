463ec5dbdb70697b50561c5117e7c401fb8c69a4
Jared Yu <qzyu999@gmail.com>
2020-12-13 20:00:48 -0800

    Updated app version

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy, event
 from datetime import datetime
 import os
+from config import setup_dic
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 
 class Clipboard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.REAL, default=0, nullable=False)
     item_name = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -18,19 +20,22 @@ class Clipboard(db.Model):
 
 @event.listens_for(Clipboard.__table__, 'after_create')
 def create_clipboard_items(*args, **kwargs):
-    db.session.add(Clipboard(item_name='Name', content='Your name'))
-    db.session.add(Clipboard(item_name='Email', content='email@address.com'))
-    db.session.add(Clipboard(item_name='Address', content='123 Blvd.'))
-    db.session.add(Clipboard(item_name='LinkedIn', content='https://www.linkedin.com/in/your-li-name/'))
-    db.session.add(Clipboard(item_name='GitHub Page', content='https://your-github-page.github.io'))
+    for index, (key, value) in enumerate(setup_dic.items()):
+        db.session.add(Clipboard(item_name=key, order_id=index, content=setup_dic[key]))
+    # db.session.add(Clipboard(item_name='Name', content='Your name'))
+    # db.session.add(Clipboard(item_name='Email', content='email@address.com'))
+    # db.session.add(Clipboard(item_name='Address', content='123 Blvd.'))
+    # db.session.add(Clipboard(item_name='LinkedIn', content='https://www.linkedin.com/in/your-li-name/'))
+    # db.session.add(Clipboard(item_name='GitHub Page', content='https://your-github-page.github.io'))
     db.session.commit()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        order_id = request.form['order_id']
         item_name = request.form['item_name']
         task_content = request.form['content']
-        new_task = Clipboard(item_name=item_name, content=task_content)
+        new_task = Clipboard(order_id=order_id, item_name=item_name, content=task_content)
 
         try:
             db.session.add(new_task)
@@ -40,7 +45,7 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        tasks = Clipboard.query.order_by(Clipboard.item_name).all()
+        tasks = Clipboard.query.order_by(Clipboard.order_id).all()
         return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
@@ -59,6 +64,7 @@ def update(id):
     task = Clipboard.query.get_or_404(id)
 
     if request.method == 'POST':
+        task.order_id = request.form['order_id']
         task.item_name = request.form['item_name']
         task.content = request.form['content']
 
